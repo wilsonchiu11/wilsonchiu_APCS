6df198b830e7417265a86ba109687bc0fdc3ac6c
wilsonchiu11 <18chiuw79@millshighsch.org>
2016-10-26 10:48:48 -0700

    10/26/16
    things i did in class

--- SplittingStrings/src/Split.java
+++ SplittingStrings/src/Split.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 public class Split 
 {
 
@@ -14,21 +14,32 @@ public class Split
 		//		it will split at the word "really" and return an array of ["I "," like "," apples!"]
 		
 		//play around with String.split! what happens if you "I reallyreally like apples".split("really") ?
+		String st = "I like apples!";
+		String[] split1 = st.split(" ");
+		System.out.println(Arrays.toString(split1));
 		
 		
+		String str = "I really like really red apples";
+		String[] split2 = str.split(" really ");
+		System.out.println(Arrays.toString(split2));
+		 
 		//Your task:
 		/*Write a method that take in a string like "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a sandwich
 		 * use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
 		 * What if it's a fancy sandwich with multiple pieces of bread?
 		*/
-		
+		String strin = "applespineapplesbreadlettustomatobaconmayohambreadcheese";
+		String[] split3 = strin.split("bread");
+		System.out.println(Arrays.toString(split3));
 		
 		//Your task pt 2:
 		/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
 		 * use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
 		 * Again, what if it's a fancy sandwich with multiple pieces of bread?
 		*/
-
+		String stri = "apples pineapples bread lettus tomato bacon mayo ham bread cheese";
+		String[] split4 = stri.split("apples");
+		System.out.println(Arrays.toString(split4));
 		
 
 	}
